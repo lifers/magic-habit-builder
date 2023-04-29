856eed9738b1ee6f57d32a8ec974f0a12b07f27d
Nagata Parama Aptana <61454123+lifers@users.noreply.github.com>
2023-04-29 13:55:31 -0400

    only take table

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ def get_blocking_times() -> Optional[list]:
 
     return blocking_times
 
+
 def generate_prompt(habits: list[str], blocking_times: Optional[list]) -> str:
     """generate string prompt"""
     # TODO: Make examples for reproducibility
@@ -49,6 +50,9 @@ def generate_prompt(habits: list[str], blocking_times: Optional[list]) -> str:
 
     prmt += "Make a formatted schedule in a table with the following format:" \
             "[time: start - end] | [activity]"
+    
+    return prmt
+
 
 def run_scraper(query: str) -> str:
     """run scraper, get answer"""
@@ -61,39 +65,25 @@ def run_scraper(query: str) -> str:
         by=By.XPATH,
         value='//textarea[@placeholder="Ask anything..."]')
     
-    driver.implicitly_wait(20)
+    driver.implicitly_wait(10)
     search_box.send_keys(query + Keys.ENTER)
     answer = driver.find_element(by=By.CLASS_NAME, value='prose')
-    # answer = WebDriverWait(driver, 15).until(lambda d: d.find_element(by=By.CLASS_NAME, value='prose'))
+    table = answer.find_element(by=By.XPATH, value='//table[@class="border w-full border-borderMain dark:border-borderMainDark"]')
 
-    return answer.text
+    return table.text
 
 
 def parse_answer(answer: str) -> str:
     """parse answer from scraper into something more manageable"""
-    # answer = answer[answer.find('Time Activity\n'):].split('\n')
-    # result = []
-    # for schedule in answer:
-    #     split_schedule = schedule.split()
-    #     try:
-    #         time_of_day = datetime.strptime(split_schedule[0] + split_schedule[1], '%I:%M%p')
-    #     except ValueError:
-    #         raise ValueError("Invalid answer from AI")
-
     return answer
 
 
 if __name__ == "__main__":
-    # habits = get_habits(3)
-    # blocking_times = get_blocking_times()
-    # prompt = generate_prompt(habits, blocking_times)
-    prompt = """I want to make 3 new habits by doing it every day. Those three habits are: 
-1. Running 5km for 60 minutes 
-2. Calling one friend 
-3. Putting aside $5 for 5 minutes 
-
-Make a schedule that works for me, and make it a csv."""
-
+    habits = get_habits(3)
+    blocking_times = get_blocking_times()
+    prompt = generate_prompt(habits, blocking_times)
+    print(prompt)
+    print('-------------------------------------------------------------------------------')
     answer = run_scraper(prompt)
     result = parse_answer(answer)
     print(result)
