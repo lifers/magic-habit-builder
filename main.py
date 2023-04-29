f4ef619ba692ac0bea63dfc03a9351397cc588dd
Nagata Parama Aptana <61454123+lifers@users.noreply.github.com>
2023-04-29 13:10:48 -0400

    try parse answer

--- main.py
+++ main.py
@@ -28,29 +28,48 @@ def generate_prompt(habits: list[str], blocking_times: Optional[list]) -> str:
 
 def run_scraper(query: str) -> str:
     """run scraper, get answer"""
-    options = EdgeOptions().add_argument('--headless')
+    options = EdgeOptions().add_argument(argument=['start-fullscreen'])
     driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=options)
+    driver.maximize_window()
 
     driver.get('https://www.perplexity.ai')
     search_box = driver.find_element(
         by=By.XPATH,
         value='//textarea[@placeholder="Ask anything..."]')
     
+    driver.implicitly_wait(20)
     search_box.send_keys(query + Keys.ENTER)
-    driver.implicitly_wait(10)
     answer = driver.find_element(by=By.CLASS_NAME, value='prose')
+    # answer = WebDriverWait(driver, 15).until(lambda d: d.find_element(by=By.CLASS_NAME, value='prose'))
 
     return answer.text
 
 
 def parse_answer(answer: str) -> str:
     """parse answer from scraper into something more manageable"""
+    # answer = answer[answer.find('Time Activity\n'):].split('\n')
+    # result = []
+    # for schedule in answer:
+    #     split_schedule = schedule.split()
+    #     try:
+    #         time_of_day = datetime.strptime(split_schedule[0] + split_schedule[1], '%I:%M%p')
+    #     except ValueError:
+    #         raise ValueError("Invalid answer from AI")
+
+    return answer
 
 
 if __name__ == "__main__":
-    habits = get_habits(3)
-    blocking_times = get_blocking_times()
-    prompt = generate_prompt(habits, blocking_times)
+    # habits = get_habits(3)
+    # blocking_times = get_blocking_times()
+    # prompt = generate_prompt(habits, blocking_times)
+    prompt = """I want to make 3 new habits by doing it every day. Those three habits are: 
+1. Running 5km for 60 minutes 
+2. Calling one friend 
+3. Putting aside $5 for 5 minutes 
+
+Make a schedule that works for me, and make it a csv."""
+
     answer = run_scraper(prompt)
     result = parse_answer(answer)
     print(result)
