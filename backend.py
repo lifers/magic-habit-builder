2c934bd28dc3229687dadf8583be334b988ac92e
Maveluke <maveluk.cy@gmail.com>
2023-04-30 06:31:27 -0400

    Some changes
    backend
    forms
    style
    change directories for home.html

--- backend.py
+++ backend.py
@@ -1,19 +1,38 @@
 from flask import Flask, render_template
-from forms import ThreeBoxForm
+from forms import ThreeBoxForm, BlockTimeForm
 
 # Create the application.
 APP = Flask(__name__)
 APP.config['SECRET_KEY'] = "121r2h3oh23h45jh23l4"
 
+habits = []
+blocks = []
+
 
 @APP.route('/', methods=['get', 'post'])
 def home():
+    return render_template("home.html")
+
+
+@APP.route('/habits', methods=['get', 'post'])
+def habits():
     form = ThreeBoxForm()
-    print(form.box1.data)
-    print(form.box2.data)
-    print(form.box3.data)
-    print(form.date.data)
-    return render_template("home.html", form=form)
+    habits.clear()
+    if not (form.box1.data is None or form.box2.data is None or form.box3.data is None):
+        habits.extend([form.box1.data, form.box2.data, form.box3.data])
+    return render_template("habits.html", form=form)
+
+
+@APP.route('/blocktimes', methods=['GET', 'POST'])
+def blocktimes():
+    form = BlockTimeForm()
+    blocks.clear()
+    for field in form.fields:
+        if not (field.begin.data is None or field.end.data is None or field.begin.data > field.end.data):
+            # print(str(field.begin.data)[:-3])
+            blocks.append((str(field.begin.data)[:-3], str(field.end.data)[:-3]))
+
+    return render_template('blocktimes.html', form=form)
 
 
 if __name__ == '__main__':
