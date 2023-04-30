c3d30aede4ff997e079403209c95baad1399164c
CuriousChum <109543738+CuriousChum@users.noreply.github.com>
2023-04-30 02:08:04 -0400

    dff

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, render_template, session
 from forms import ThreeBoxForm
+import main as mn
 
 # Create the application.
 APP = Flask(__name__)
@@ -14,6 +15,24 @@ def home():
     print(form.box3.data)
     print(form.date.data)
     return render_template("home.html", form=form)
+
+
+@APP.route('/result', methods=['get', 'post'])
+def result():
+    if "habits" in session and "schedule" in session:
+        habits = session["habits"]
+        schedule = session["schedule"]
+        prompt = mn.generate_prompt(habits, schedule)
+        reply = mn.run_scraper(prompt)
+        raw_res = mn.parse_answer(reply)
+        res = raw_res
+        return f"{res}"
+    elif "schedule" not in session:
+        return redirect(url_for(""))
+    elif "habits" not in session:
+        return redirect(url_for(""))
+    else:
+        return redirect(url_for("home"))
 
 
 if __name__ == '__main__':
