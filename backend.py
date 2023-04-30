55f22c2fd05a5983991da0c7d53741bbeb156f58
Christoffer Tan <tanchristoffer@gmail.com>
2023-04-30 06:08:52 -0400

    edit home

--- backend.py
+++ backend.py
@@ -1,23 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from forms import ThreeBoxForm
+from temp_main import get_blocking_times, generate_prompt, run_scraper, parse_answer
 
 # Create the application.
 APP = Flask(__name__)
 APP.config['SECRET_KEY'] = "121r2h3oh23h45jh23l4"
 
 
-@APP.route('/', methods=['get', 'post'])
-def home():
+# @APP.route('/', methods=['get', 'post'])
+# def home():
+#     # TODO: Implement the home page
+#     pass
+#
+
+@APP.route('/habits', methods=['get', 'post'])  # TODO: Add hyperlink to habits "page"
+# TODO: change route to #habits
+def habit():
     form = ThreeBoxForm()
-    print(form.box1.data)
-    print(form.box2.data)
-    print(form.box3.data)
-    print(form.date.data)
-    return render_template("home.html", form=form)
+    if not (form.box1.data is None or form.box2.data is None or form.box3.data is None):
+        session['habits'] = [form.box1.data, form.box2.data, form.box3.data]
+    return render_template("habits.html", form=form)
 
 
 if __name__ == '__main__':
     APP.debug = True
     APP.run()
-
 
